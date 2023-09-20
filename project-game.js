eff4f20053347dea1e00211033f852bc4c8295e8
TheBionicWoman <142552817+TheBionicWoman@users.noreply.github.com>
2023-09-20 13:26:12 -0400

    Add function that plays a single round of rock, paper, scissors

--- project-game.js
+++ project-game.js
@@ -1,12 +1,42 @@
 function getComputerChoice() {
     const choiceArray = ['rock', 'paper', 'scissors'];
-    const randomComputerSelection = choiceArray[Math.floor(Math.random() * choiceArray.length)];
-    console.log(randomComputerSelection)
+    return RandomComputerSelection = choiceArray[Math.floor(Math.random() * choiceArray.length)];
+    /* console.log(randomComputerSelection) */
 }
 
-function playerRound() {
-    
+const playerSelection = 'scissors';
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection != 'rock') {
+        if (computerSelection === 'paper') {
+            return 'You lose! Paper beats Rock';
+        } else if (computerSelection === 'scissors') {
+            return 'You win! Rock beats Scissors'
+        }
+    } else if (playerSelection === 'rock' && computerSelection === 'rock'){
+        return "You both chose rock! It's a tie"
+    }
+
+    if (playerSelection === 'paper' && computerSelection != 'paper') {
+        if (computerSelection === 'rock') {
+            return 'You win! Paper beats Rock';
+        } else if (computerSelection === 'scissors') {
+            return 'You lose! Scissors beat Paper';
+        }
+    } else if (playerSelection === 'paper' && computerSelection == 'paper') {
+        return "You both chose paper! It's a tie";
+    }
+
+    if (playerSelection === 'scissors' && computerSelection != 'scissors') {
+        if (computerSelection === 'rock') {
+            return 'You lose! Rock beats Scissors';
+        } else if (computerSelection === 'paper') {
+            return 'You win! Scissors beat Paper';
+        }
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors'){
+        return "You both chose scissors! It's a tie";
+    }
 }
 
-
-getComputerChoice()
+console.log(playRound(playerSelection, computerSelection))
