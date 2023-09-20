6302e07542969143a0298f2e159a11b739c19a13
TheBionicWoman <142552817+TheBionicWoman@users.noreply.github.com>
2023-09-20 12:22:16 -0400

    Create a function that will randomly return rock, paper, or scissors.

---
+++ project-game.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    const choiceArray = ['rock', 'paper', 'scissors'];
+    const randomComputerSelection = choiceArray[Math.floor(Math.random() * choiceArray.length)];
+    console.log(randomComputerSelection)
+}
+
+function playerRound() {
+    
+}
+
+
+getComputerChoice()
